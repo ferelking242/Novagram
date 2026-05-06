2f0c4418fc308759c62773cd77e2008a68d9c7f9
FERELKING242 <111065891+ferelking242@users.noreply.github.com>
2026-05-07 00:45:25 +0100

    fix: remove redundant JVM getter methods causing platform declaration clash in NicegramClientHelper.kt

--- novagram-stubs/assistant-core/src/main/java/com/appvillis/feature_nicegram_client/NicegramClientHelper.kt
+++ novagram-stubs/assistant-core/src/main/java/com/appvillis/feature_nicegram_client/NicegramClientHelper.kt
@@ -4,7 +4,6 @@ import com.appvillis.feature_nicegram_client.domain.NgClientRemoteConfigRepo
 
 object NicegramClientHelper {
     private val _preferences = Preferences()
-    var preferences: Preferences? = _preferences
 
     @JvmStatic fun getPreferences(): Preferences = _preferences
 
@@ -18,13 +17,6 @@ object NicegramClientHelper {
         @JvmField var canShareChannels: Boolean = true
         @JvmField var canShareBots: Boolean = true
         @JvmField var canShareStickers: Boolean = true
-
-        fun getCanShareChannels(): Boolean = canShareChannels
-        fun setCanShareChannels(value: Boolean) { canShareChannels = value }
-        fun getCanShareBots(): Boolean = canShareBots
-        fun setCanShareBots(value: Boolean) { canShareBots = value }
-        fun getCanShareStickers(): Boolean = canShareStickers
-        fun setCanShareStickers(value: Boolean) { canShareStickers = value }
     }
 
     class ReferralDrawUseCase {
