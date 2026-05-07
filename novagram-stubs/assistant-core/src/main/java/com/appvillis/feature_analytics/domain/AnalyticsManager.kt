e7d70c2b133a4be1378c3200a4943c9a8552f9df
FERELKING242 <111065891+ferelking242@users.noreply.github.com>
2026-05-07 01:04:04 +0100

    fix: AnalyticsManager.kt - resolve nicegram-features compilation errors

--- novagram-stubs/assistant-core/src/main/java/com/appvillis/feature_analytics/domain/AnalyticsManager.kt
+++ novagram-stubs/assistant-core/src/main/java/com/appvillis/feature_analytics/domain/AnalyticsManager.kt
@@ -1,10 +1,13 @@
 package com.appvillis.feature_analytics.domain
 
 import com.appvillis.feature_analytics.data.AnalyticsEvent
+import com.appvillis.feature_analytics.data.AnalyticsTrackEvent
 import com.appvillis.feature_analytics.data.AnalyticsValue
 
 interface AnalyticsManager {
     fun logEvent(name: String)
     fun logEvent(name: String, params: Map<String, AnalyticsValue>)
     fun logEvent(event: AnalyticsEvent)
+    fun logEvent(event: AnalyticsTrackEvent)
+    fun logEventOncePerSession(name: String)
 }
