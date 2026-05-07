3f2699a23f5efee678e8296f8bb137a74e52227a
FERELKING242 <111065891+ferelking242@users.noreply.github.com>
2026-05-07 01:04:05 +0100

    fix: NgWidgetsEntryPoint.kt - resolve nicegram-features compilation errors

--- novagram-stubs/assistant-core/src/main/java/com/appvillis/feature_chat_widgets/NgWidgetsEntryPoint.kt
+++ novagram-stubs/assistant-core/src/main/java/com/appvillis/feature_chat_widgets/NgWidgetsEntryPoint.kt
@@ -3,9 +3,12 @@ package com.appvillis.feature_chat_widgets
 import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
 
 @EntryPoint
 @InstallIn(SingletonComponent::class)
 interface NgWidgetsEntryPoint {
     fun ngWidgetsHelper(): NgWidgetsHelper
+    fun appScope(): CoroutineScope
+    fun pinnedBannerManager(): PinnedBannerManager
 }
